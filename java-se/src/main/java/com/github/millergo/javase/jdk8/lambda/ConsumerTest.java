8eb7d3da2bb8f663b37378e43c55230269a79482
Miller.Shan <miller.shan@aliyun.com>
2023-06-05 23:38:14 +0800

    docs: lambda lesson1

---
+++ java-se/src/main/java/com/github/millergo/javase/jdk8/lambda/ConsumerTest.java
@@ -0,0 +1,27 @@
+package com.github.millergo.javase.jdk8.lambda;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Consumer 函数接口的含义维接受一个参数，不返回结构。
+ */
+public class ConsumerTest {
+    @Test
+    void testAndThen() {
+        List<String> myFamily = Arrays.asList("Miller", "Mila", "Vicky");
+        Consumer<String> consumer1 = (param) -> {
+            String toUpperCase = param.toUpperCase();
+            System.out.println(toUpperCase);
+        };
+        Consumer<String> consumer2 = (param) -> {
+            CharSequence charSequence = param.subSequence(0, param.length() - 1);
+            System.out.println(charSequence);
+        };
+        // Consumer 接口中的 andThen 方法接受一个 Consumer 参数。含义是先执行自身然后在执行参数中的 Consumer 函数接口的实现。
+        consumer1.andThen(consumer2).accept("Miller");
+    }
+}
---
+++ java-se/src/main/java/com/github/millergo/javase/jdk8/lambda/LambdaTest.java
@@ -0,0 +1,61 @@
+package com.github.millergo.javase.jdk8.lambda;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * lambda 表达式（函数式编程）在Java中实际是一个对象，只是这个对象的创建方式可以是通过lambda表达式、方法引用、构造器方式。
+ * lambda 语法：通过箭头区分，箭头左边代表方法的参数， 右边代表具体实现。<br>
+ * 1: (Type1 param1, Type2 param2,...) -> { statement....} <br>
+ * 2: param1, param2 -> {statement...} <br>
+ * 3: param1 -> {statement...} <br>
+ * 4: () -> {statement...} <br>
+ *
+ * @see FunctionalInterface
+ */
+public class LambdaTest {
+
+    @Test
+    @DisplayName("lambda")
+    void lambdaShouldBeJavaObject() {
+        List<String> myFamily = Arrays.asList("Miller", "Mila", "Vicky");
+        myFamily.forEach(value -> System.out.println(value));
+        // 通过一个引用类型指向一个对象
+        Consumer consumer = param -> {
+        };
+        System.out.println(consumer.getClass());
+        System.out.println(consumer.getClass().getSimpleName());
+        System.out.println(consumer.getClass().getSuperclass());
+        System.out.println(consumer.getClass().getInterfaces()[0]);
+    }
+
+    @Test
+    void myFunction() {
+        MyFunction myFunction = (paramOne) -> {
+        };
+        System.out.println(myFunction.getClass().getTypeName());
+        // 如果lambda代码块只有一行可以省略（）、{}。 默认可以省略类型声明，编译器会自动推断类型。
+        MyFunction<String> myFunction2 = (String paramOne) -> {
+            // 传递行为：将参数转换为大写
+            String result = paramOne.toUpperCase();
+            System.out.println(result);
+        };
+        myFunction2.method("Miller");
+    }
+}
+
+/**
+ * 自定义函数式接口。
+ * 函数式编程与面向对象最大的不同在于，函数式编程传递的是行为（具体实现由客户端决定），而面向对象编程传递的是数据（具体实现已预先定义）。
+ */
+@FunctionalInterface
+interface MyFunction<P> {
+    /**
+     * 接受一个参数不返回结果
+     */
+    void method(P p);
+}
